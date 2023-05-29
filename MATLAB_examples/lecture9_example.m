5d5ff4e98013c38f9e3126546d460bdaa950a9f8
Stu Blair <stu314159@gmail.com>
2023-05-29 13:24:59 -0400

    completed lecture 9

--- MATLAB_examples/lecture9_example.m
+++ MATLAB_examples/lecture9_example.m
@@ -6,7 +6,7 @@ close 'all'
 % this version will try and do without symbolic series
 
 %% Parameters
-n = 25;
+n = 6;
 xMin = 0; xMax = 5;
 
 %% Call a local Function to Generate Partial Series Solution
@@ -18,7 +18,7 @@ fplot(y,[xMin, xMax],'linewidth',2);
 title_str = sprintf('Lecture 9 Series n = %d',n);
 title(title_str,'fontsize',18,'fontweight','bold');
 xlabel('X','fontsize',16,'fontweight','bold');
-ylabel('Y(X)','fontsize',16,'fontweight','bold');
+ylabel('U(X)','fontsize',16,'fontweight','bold');
 set(gca,'fontsize',12,'fontweight','bold');
 grid on
 
@@ -43,7 +43,7 @@ hold off
 title_str = sprintf('Power Series Comparison n = %d',n);
 title(title_str,'fontsize',18,'fontweight','bold');
 xlabel('X','fontsize',16,'fontweight','bold');
-ylabel('Y(X)','fontsize',16,'fontweight','bold');
+ylabel('U(X)','fontsize',16,'fontweight','bold');
 set(gca,'fontsize',12,'fontweight','bold');
 legend('Power Series','ODE45');
 grid on
@@ -58,6 +58,50 @@ ylabel('Relative Error','FontSize',12,'FontWeight','bold');
 sol_rel_err_norm = norm(sol_rel_err,2);
 fprintf('The 2-norm of the relative error = %g',...
     sol_rel_err_norm);
+
+%%
+u5 = generate_series(5);
+u10 = generate_series(10);
+u15 = generate_series(15);
+u20 = generate_series(20);
+
+xPlt = linspace(xMin,xMax,1000);
+figure(5)
+plot(xPlt,u5(xPlt),'-b',...
+    xPlt,u10(xPlt),'-c',...
+    xPlt,u15(xPlt),'-k',...
+    xPlt,u20(xPlt),'-g',...
+    t,y_num(:,1),'--r','linewidth',3);
+title('Power Series Solution Accuracy',...
+    'fontsize',16,'fontweight','bold');
+xlabel("X");
+ylabel("U(X)");
+legend('n = 5','n=10','n=15','n=20','ODE45');
+grid on
+set(gca,'fontsize',12,'fontweight','bold')
+
+
+n_vals = [5 10 15 20];
+rel_err_vals = nan(1,4);
+re_fun = @(u,t) norm(u(t) - y_num(:,1),2)/norm(y_num(:,1),2);
+rel_err_vals(1) = re_fun(u5,t);
+rel_err_vals(2) = re_fun(u10,t);
+rel_err_vals(3) = re_fun(u15,t);
+rel_err_vals(4) = re_fun(u20,t);
+
+figure(6)
+plot(n_vals,rel_err_vals,'-sb',...
+    'linewidth',3,...
+    'markersize',10)
+title('Convergence of Power Series Solution',...
+    'fontsize',14,'fontweight','bold');
+xlabel('Number of Power Series Terms','fontsize',12,...
+    'FontWeight','bold');
+ylabel('Relative Error');
+grid on
+set(gca,'fontsize',10,'fontweight','bold');
+
+
 
 %% Local function to generate the series
 function  y = generate_series(n)
@@ -74,7 +118,7 @@ k = 1;
 C1(k+2) = (C1(k)+c1_0)/((k+1)*(k+2));
 
 % compute the coefficients
-for k = 2:(n-3)
+for k = 2:(n-2)
     C1(k+2) = (C1(k) + C1(k-1))/((k+1)*(k+2));
 end
 
@@ -93,7 +137,7 @@ C2(2) = c2_0/2; % again, c2 = c0/2
 k = 1;
 C2(k+2) = (C2(k) + c2_0)/((k+1)*(k+2));
 % compute the coefficients
-for k = 2:(n-3)
+for k = 2:(n-2)
    C2(k+2)= (C2(k) + C2(k-1))/((k+1)*(k+2)); 
 end
 
