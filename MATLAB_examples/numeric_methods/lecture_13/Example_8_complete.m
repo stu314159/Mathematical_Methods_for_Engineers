b8695d4bed9c7149835b6d0a6a47d9905733a125
Stu Blair <stu314159@gmail.com>
2023-08-01 11:36:13 -0400

    added example to end of lecture 13n

--- MATLAB_examples/numeric_methods/lecture_13/Example_8_complete.m
+++ MATLAB_examples/numeric_methods/lecture_13/Example_8_complete.m
@@ -9,10 +9,14 @@ d = [22 26 27 30 34 33 33.5 37 42]';
 
 %% Plot the data
 figure(1)
-plot(I,d,'sk','markersize',10,'linewidth',2);
-title('Raw Data','fontsize',18,'fontweight','bold');
-xlabel('Current Flow (nA)','fontsize',16,'fontweight','bold');
-ylabel('Fiber Diameter (\mum)','fontsize',16,'fontweight','bold');
+plot(I,d,'sk','markersize',10,...
+    'linewidth',2);
+title('Raw Data','fontsize',18,...
+    'fontweight','bold');
+xlabel('Current Flow (nA)',...
+    'fontsize',16,'fontweight','bold');
+ylabel('Fiber Diameter (\mum)',...
+    'fontsize',16,'fontweight','bold');
 grid on
 set(gca,'fontsize',12,'fontweight','bold');
 axis([250 675 20 45]);
@@ -31,10 +35,14 @@ linEst = @(x) c(1) + c(2)*x;
 % plot the fit line
 figure(2)
 plot(I,d,'sk',...
-    I,linEst(I),'-r','linewidth',3,'markersize',10);
-title('Raw Data','fontsize',18,'fontweight','bold');
-xlabel('Current Flow (nA)','fontsize',16,'fontweight','bold');
-ylabel('Fiber Diameter (\mum)','fontsize',16,'fontweight','bold');
+    I,linEst(I),'-r','linewidth',3,...
+    'markersize',10);
+title('Linear Fit','fontsize',18,...
+    'fontweight','bold');
+xlabel('Current Flow (nA)','fontsize',16,...
+    'fontweight','bold');
+ylabel('Fiber Diameter (\mum)','fontsize',16,...
+    'fontweight','bold');
 grid on
 set(gca,'fontsize',12,'fontweight','bold');
 axis([250 675 20 45]);
@@ -58,7 +66,7 @@ figure(3)
 plot(I,d,'sk',...
     I,linEst(I),'-r',...
     I,quadEst(I),'-g','linewidth',3,'markersize',10);
-title('Raw Data','fontsize',18,'fontweight','bold');
+title('Second-Order Fit','fontsize',18,'fontweight','bold');
 xlabel('Current Flow (nA)','fontsize',16,'fontweight','bold');
 ylabel('Fiber Diameter (\mum)','fontsize',16,'fontweight','bold');
 grid on
@@ -67,7 +75,7 @@ axis([250 675 20 45]);
 
 %% Least Squares - (problem 6.9)
 
-fprintf('\n\n Least Squares - non-monomial \n');
+fprintf('\n\n Least Squares - Model Fit \n');
 X = [I.^0 I.^0.5];
 b = d;
 c = (X'*X)\(X'*b);
@@ -82,7 +90,7 @@ plot(I,d,'sk',...
     I,linEst(I),'-r',...
     I,quadEst(I),'-g',...
     I,est3(I),'-c','linewidth',3,'markersize',10);
-title('Raw Data','fontsize',18,'fontweight','bold');
+title('Model Fit','fontsize',18,'fontweight','bold');
 xlabel('Current Flow (nA)','fontsize',16,'fontweight','bold');
 ylabel('Fiber Diameter (\mum)','fontsize',16,'fontweight','bold');
 grid on
