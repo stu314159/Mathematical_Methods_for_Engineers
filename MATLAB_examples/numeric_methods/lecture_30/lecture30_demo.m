2db3c89ff51a45c42728b892d45190d443984511
Stu Blair <stu314159@gmail.com>
2024-05-31 09:15:29 -0400

    finish initial draft of lecture 30n

--- MATLAB_examples/numeric_methods/lecture_30/lecture30_demo.m
+++ MATLAB_examples/numeric_methods/lecture_30/lecture30_demo.m
@@ -3,7 +3,7 @@ clear
 clc
 close 'all'
 
-%% Example 1
+%% Parameters
 R = 1.5e-2; % m, radius of fuel
 w = 3.0e-3; % m, thickness of cladding
 k = 16.75; % W/(m-K), thermal conductivity of clad
@@ -13,28 +13,33 @@ Q2 = 6.32e5; % W/m^2, Heat flux due to heat produced in fuel.
 T_inf = 423; % K, temperature of water flowing on cladding
 h = 1e4; % W/(m^2-K), convective heat transfer coefficient
 
+%% Encode the governing equation and boundary conditions
+
 F = @(r,T) [T(2); -(1./r)*T(2) - Q./(k*r)*exp(-r/R)];
 bcfun = @(Ta,Tb) [Ta(2)+ Q2/k; ...
     Tb(2) + (h/k)*(Tb(1)-T_inf)];
+
+%% Establish initial mesh and solution estimate
 rMin = R; rMax = R+w; nR = 200;
 Tguess = [T_inf 0];
 solinit = bvpinit(linspace(rMin,rMax,nR),Tguess);
 
+%% Solve and plot the result
 sol = bvp5c(F,bcfun,solinit);
 
 figure(1)
 plot(sol.x,sol.y(1,:),'-b','linewidth',2);
-title('Fuel Clad Temperature','fontsize',14,...
+title('Fuel Clad Temperature','fontsize',16,...
     'fontweight','bold');
-xlabel('R [m]','fontsize',12,'fontweight','bold');
-ylabel('T(R) [K]','fontsize',12,'fontweight','bold');
+xlabel('R [m]','fontsize',14,'fontweight','bold');
+ylabel('T(R) [K]','fontsize',14,'fontweight','bold');
 grid on
-set(gca,'fontsize',10,'fontweight','bold');
+set(gca,'fontsize',12,'fontweight','bold');
 
 %% Parameter Example
 %%
 % 
-%  Suppose that Q could be modified.  We migth conceive of a problem where
+%  Suppose that Q could be modified.  We might conceive of a problem where
 %  we want to find the value of Q such that the T(r=R) hits a specified
 %  value.  MATLAB's built-in solvers provide functionality that allows this
 %  type of analysis.
